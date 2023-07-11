1f99b152b135221c6d69c520b66880e22e4137a6
simonvalentino <simontvalentino@gmail.com>
2023-07-11 15:02:13 -0400

    Added github url to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     version="1.0",
     author="Simon Valentino",
     author_email="simontvalentino@gmail.com",
+    url="https://github.com/SimonValentino/tictactoetools.git",
     description="Creatively build Tic-Tac-Toe matches and store their data in a database of all users.",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
